7e682b5aa030de44dfc3f4807979fb8a06c27cbf
Raphael SAMPAN <raphaelsampan@gmail.com>
2019-07-19 02:04:35 +0200

    ba-2 basic server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "@babel/polyfill"
+import http from "http"
+
+const requestHandler = function (req, res) {
+
+  res.writeHead(200, {
+    'Content-Type': 'Text/plain'
+  })
+  res.end('Hello Raphael')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(8083)
