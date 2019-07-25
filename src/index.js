88a392214ea671442bb3a408435a5db6654d55bf
Raphael SAMPAN <raphaelsampan@gmail.com>
2019-07-26 01:43:49 +0200

    Add basic server

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "@babel/polyfill"
+import http from "http"
+
+const requestHandler = function(req, res) {
+
+  res.writeHead(200, {
+     'Content-Type': 'Text/plain'
+  })
+  res.end("Hello Raphael detaillons les processus de dev nodeJS et React via docker")
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(9099)
