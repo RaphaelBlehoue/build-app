f4ea52896fa1c89076783c23ca771a89439d2696
Raphael SAMPAN <raphaelsampan@gmail.com>
2019-07-29 00:55:23 +0200

    Add eslint config and hooks

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import "@babel/polyfill"
-import http from "http"
+import '@babel/polyfill';
+import http from 'http';
 
-const requestHandler = function(req, res) {
-
-  res.writeHead(200, {
-     'Content-Type': 'Text/plain'
-  })
-  res.end("Hello Raphael detaillons les processus de dev nodeJS et React via docker")
+function requestHandler(req, res) {
+  res.writeHead(200, { 'Content-Type': 'Text/plain' });
+  res.end('Hello Raphael detaillons les processus de dev nodeJS et React via docker');
 }
 
-const server = http.createServer(requestHandler)
-server.listen(9099)
+const server = http.createServer(requestHandler);
+server.listen(9099);
