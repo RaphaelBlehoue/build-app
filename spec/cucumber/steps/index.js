bf7a4952d038e2aa1753fb534d88d2060daf5de4
Raphael SAMPAN <raphaelsampan@gmail.com>
2019-08-05 00:47:10 +0200

    Add basic config cucumber js

---
+++ spec/cucumber/steps/index.js
@@ -0,0 +1,25 @@
+import { When, Then } from 'cucumber';
+
+When('the client creates a POST request to \/users', function () {
+  return 'pending';
+});
+
+When('attaches a generic empty payload', function () {
+  return 'pending';
+});
+
+When('send the request', function () {
+  return 'pending';
+});
+
+Then('our API should response with a {int} HTTP status code', function (int) {
+  return 'pending';
+});
+
+Then('the payload of response should be a JSON object', function () {
+  return 'pending';
+});
+
+Then('contains a message property which says {string}', function (string) {
+  return 'pending';
+});
