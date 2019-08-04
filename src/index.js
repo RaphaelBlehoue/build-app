a7e25194d942c230f5da3e9dda69f53f3b10e369
Raphael Blehoue <raphaelsampan@gmail.com>
2019-08-05 00:50:06 +0200

    Features/ba 5 config cucumber env (#15)
    * Merging init config and basic server create (#8)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Feature/ba 1 automating dev nodemon (#10)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Add nvmrc file (#9)
    
    * Develop (#12)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Add nvmrc file (#9)
    
    * Revert 10 feature/ba 1 automating dev nodemon (#11)
    
    * Merging init config and basic server create (#8)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Feature/ba 1 automating dev nodemon (#10)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Add nvmrc file (#9)
    
    * Revert "Feature/ba 1 automating dev nodemon (#10)"
    
    This reverts commit abd71200ab7fcccb14041c86e434409876eed8a2.
    
    * Add basic config cucumber js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import '@babel/polyfill';
 import http from 'http';
 
-function requestHandler(res) {
-  res.writeHead(200, { 'Content-Type': 'Text/plain' });
+const requestHandler = function (req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'Text/plain',
+  });
   res.end('Hello Raphael detaillons les processus de dev nodeJS et React via docker');
-}
+};
 
 const server = http.createServer(requestHandler);
 server.listen(9099);
