7582788ba5fba91560645a085ad6d473e601a2ec
Raphael Blehoue <raphaelsampan@gmail.com>
2019-08-27 21:26:30 +0200

    Develop (#19)
    * Features/ba 5 config cucumber env (#15)
    
    * Merging init config and basic server create (#8)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Feature/ba 1 automating dev nodemon (#10)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Add nvmrc file (#9)
    
    * Develop (#12)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Add nvmrc file (#9)
    
    * Revert 10 feature/ba 1 automating dev nodemon (#11)
    
    * Merging init config and basic server create (#8)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Feature/ba 1 automating dev nodemon (#10)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Add nvmrc file (#9)
    
    * Revert "Feature/ba 1 automating dev nodemon (#10)"
    
    This reverts commit abd71200ab7fcccb14041c86e434409876eed8a2.
    
    * Add basic config cucumber js
    
    * Features : ba create user main (#17)
    
    * Merging init config and basic server create (#8)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Feature/ba 1 automating dev nodemon (#10)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Add nvmrc file (#9)
    
    * Develop (#12)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Add nvmrc file (#9)
    
    * Revert 10 feature/ba 1 automating dev nodemon (#11)
    
    * Merging init config and basic server create (#8)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Feature/ba 1 automating dev nodemon (#10)
    
    * ba-1 Init config
    
    * ba-2 basic server (#6)
    
    * Add basic server (#7)
    
    Merging
    
    * Add nvmrc file (#9)
    
    * Revert "Feature/ba 1 automating dev nodemon (#10)"
    
    This reverts commit abd71200ab7fcccb14041c86e434409876eed8a2.
    
    * Debbuger VSCODE in stand by to fix
    
    * isolate context for each scenario

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,25 +1,48 @@
+import superagent from 'superagent';
 import { When, Then } from 'cucumber';
 
-When('the client creates a POST request to \/users', function () {
-  return 'pending';
+const port = process.env.PORT || 9099;
+const URL_API = `localhost:${port}`;
+
+When('the client creates a POST request to /users', function () {
+  this.request = superagent('POST', `${URL_API}/users`);
 });
 
 When('attaches a generic empty payload', function () {
-  return 'pending';
+  return undefined;
 });
 
-When('send the request', function () {
-  return 'pending';
+When('send the request', function (callback) {
+  this.request
+    .then((response) => {
+      this.response = response.res;
+      callback();
+    })
+    .catch((error) => {
+      this.response = error.response;
+      callback();
+    });
 });
 
-Then('our API should response with a {int} HTTP status code', function (int) {
-  return 'pending';
+Then('our API should response with a 400 HTTP status code', function () {
+  if (this.response.statusCode !== 400) throw new Error();
 });
 
 Then('the payload of response should be a JSON object', function () {
-  return 'pending';
+  // check Content type Header
+  const contentType = this.response.headers['Content-Type'] || this.response.headers['content-type'];
+  if (!contentType || !contentType.includes('application/json')) {
+    throw new Error('Response not of Content-Type application/json');
+  }
+  try {
+    this.responsePayload = JSON.parse(this.response.text);
+  } catch (e) {
+    throw new Error('Response not a valid JSON object');
+  }
 });
 
-Then('contains a message property which says {string}', function (string) {
-  return 'pending';
+Then('contains a message property which says "Payload should not be empty"', function () {
+  if (this.responsePayload.message !== 'Payload should not be empty') {
+    throw new Error();
+  }
 });
